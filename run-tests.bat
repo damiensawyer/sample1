f9fb2507e5cd42e9ca71a3289c8484a0066b8978
Damien Sawyer <damiensawyer@gmail.com>
2025-05-19 11:54:17 +0100

    run benchmarks

--- run-tests.bat
+++ run-tests.bat
@@ -8,13 +8,19 @@ echo.
 echo Running tests in v2...
 cd v2
 dotnet test
-cd ..
+echo Running benchmarks in v2...
+cd tests\DepthChart.Benchmarks.tests
+dotnet run --configuration RELEASE
+cd ..\..\..
 
 echo.
 echo Running tests in v3...
 cd v3
 dotnet test
-cd ..
+echo Running benchmarks in v3...
+cd tests\DepthChart.Benchmarks.tests
+dotnet run --configuration RELEASE
+cd ..\..\..
 
 echo.
 echo All tests completed
