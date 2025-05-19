4698d5885f41c6a15b6abc11634af5775b475c32
Damien Sawyer <damiensawyer@gmail.com>
2025-05-19 21:50:11 +0100

    change bat file to use sln files

--- run-tests.bat
+++ run-tests.bat
@@ -1,13 +1,13 @@
 @echo off
 echo Running tests in v1...
 cd v1
-dotnet test
+dotnet test SportsbetTestV1.sln
 cd ..
 
 echo.
 echo Running tests in v2...
 cd v2
-dotnet test
+dotnet test SportsbetTestV2.sln
 echo Running benchmarks in v2...
 cd tests\DepthChart.Benchmarks.tests
 dotnet run --configuration RELEASE
@@ -16,7 +16,7 @@ cd ..\..\..
 echo.
 echo Running tests in v3...
 cd v3
-dotnet test
+dotnet test SportsbetTestV3.sln
 echo Running benchmarks in v3...
 cd tests\DepthChart.Benchmarks.tests
 dotnet run --configuration RELEASE
